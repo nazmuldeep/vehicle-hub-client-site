a240ae830e0df171f17253579e1bfc2ab2c07cbf
nazmuldip <nazmuldip21@gmail.com>
2022-11-28 00:46:20 +0600

    dashboard folder structure added

---
+++ src/Pages/DashBoard/Users/Users.js
@@ -0,0 +1,32 @@
+import React, { useContext, } from 'react';
+import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
+const Users = () => {
+    const { user } = useContext(AuthContext)
+    const { displayName, email, photoURL, } = user
+
+    return (
+        <div>
+            <div>
+                <h1 className='text-3xl font-bold my-10 bg-cyan-200 rounded-3xl py-10 uppercase text-center'>This is  {displayName} Profile</h1>
+            </div>
+            <div className='flex justify-evenly my-10'>
+                <div className="avatar">
+                    <div className="w-24 rounded-full">
+                        <img src={photoURL} alt='' />
+                    </div>
+                </div>
+                <div className="">
+                    <div className="">
+                        <h1 className='text-3xl font-semibold'>{displayName}</h1>
+                    </div>
+                    <div className="">
+                        <h1 className='text-3xl font-semibold'>{email}</h1>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Users;

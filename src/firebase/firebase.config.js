dd189bc6ffb2168991da856b27d4ef1988c6bb3f
nazmuldip <nazmuldip21@gmail.com>
2022-11-28 00:35:43 +0600

    package install and context set

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAufxwQz6ncgaSpOvDBaom7__JrFK1xoGM",
+    authDomain: "vehicle-hub-83afe.firebaseapp.com",
+    projectId: "vehicle-hub-83afe",
+    storageBucket: "vehicle-hub-83afe.appspot.com",
+    messagingSenderId: "290357088137",
+    appId: "1:290357088137:web:691b9ebb9832651c0bdb58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

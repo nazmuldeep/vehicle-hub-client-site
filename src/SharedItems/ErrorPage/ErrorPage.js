debc77889ec0c7320a7fb6d2ec35340eeb9140cc
nazmuldip <nazmuldip21@gmail.com>
2022-11-28 12:53:26 +0600

    error page update

--- src/SharedItems/ErrorPage/ErrorPage.js
+++ src/SharedItems/ErrorPage/ErrorPage.js
@@ -6,11 +6,11 @@ const ErrorPage = () => {
         <div>
             <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
                 <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
-                    <img src="https://mir-s3-cdn-cf.behance.net/projects/404/edceab155269671.Y3JvcCwyMTQxLDE2NzUsNDcsMA.png" alt="" />
+                    <img src="https://drudesk.com/sites/default/files/2018-02/404-error-page-not-found.jpg" alt="" />
                     <div className='max-w-md text-center'>
 
                         <p className='text-2xl font-semibold md:text-3xl mb-8'>
-                            Opps! something is wrong.........
+                            OPS SOMETHING WENT WRONG ...
                         </p>
                         <Link
                             to='/'
